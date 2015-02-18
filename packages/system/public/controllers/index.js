5c3ab5e86a19f6430682edd046b74b7afc48d95a
Frank Ortmann <frank@scshgroup.com>
2015-02-18 12:31:19 +0200

    Initial commit with some pdfmake stuff added

--- packages/system/public/controllers/index.js
+++ packages/system/public/controllers/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
-  function($scope, Global) {
+angular.module('mean.system').controller('IndexController', ['$scope', 'Global', 'pdfMake',
+  function($scope, Global, pdfMake) {
     $scope.global = Global;
     $scope.sites = {
       'makeapoint':{
@@ -54,5 +54,37 @@ angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
         console.log('currentSlide:', currentSlide);
       }
     });
+
+      $scope.createPDF = function() {
+          var docDefinition = {
+              content: [
+                  // if you don't need styles, you can use a simple string to define a paragraph
+                  'This is a standard paragraph, using default style',
+
+                  // using a { text: '...' } object lets you set styling properties
+                  { text: 'This paragraph will have a bigger font', fontSize: 15 },
+
+                  // if you set the value of text to an array instead of a string, you'll be able
+                  // to style any part individually
+                  {
+                      text: [
+                          'This paragraph is defined as an array of elements to make it possible to ',
+                          { text: 'restyle part of it and make it bigger ', fontSize: 15 },
+                          'than the rest.'
+                      ]
+                  }
+              ]
+          };
+
+          // open the PDF in a new window
+          pdfMake.createPdf(docDefinition).open();
+
+          // print the PDF (not working in this version, will be added back in a couple of days)
+          // pdfMake.createPdf(docDefinition).print();
+
+          // download the PDF
+          pdfMake.createPdf(docDefinition).download();
+      };
+
   }
 ]);
